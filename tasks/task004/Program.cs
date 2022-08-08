4c0dff37ebac2d238f79687de8fbdd393b9a274e
krenartem <krenartem@yandex.ru>
2022-08-08 22:31:04 +0300

    add Task 004

---
+++ tasks/task004/Program.cs
@@ -0,0 +1,23 @@
+﻿int numberA = 9;
+int numberB = 45;
+int numberC = 8;
+int max;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+
+else
+{
+    if (numberB > numberC)
+    {
+        max = numberB;
+    }
+    else
+    {
+        max = numberC;
+    }
+}
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
