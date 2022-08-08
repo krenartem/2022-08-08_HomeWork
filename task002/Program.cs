85e20ba85cada288e0c04db38af19a2befc44b6e
krenartem <krenartem@yandex.ru>
2022-08-08 22:17:50 +0300

    add Task 002

---
+++ task002/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = 5;
+int numberB = 7;
+if (numberA > numberB)
+{
+    int max = numberA;
+}
+
+else
+{
+  int max = numberB;
+}
+
+Console.Write("MAX = ");
+Console.Write(max);
+
---
+++ task002/task002/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = -9;
+int numberB = -3;
+int max;
+if (numberA < numberB)
+{
+   max = numberB;
+}
+
+else
+{
+    max = numberA;
+}
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
