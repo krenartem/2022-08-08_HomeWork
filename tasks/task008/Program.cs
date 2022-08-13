a4fa08c42ca59970eca0603dd6a388034d7af302
krenartem <krenartem@yandex.ru>
2022-08-13 21:09:24 +0300

    add task008

--- tasks/task008/Program.cs
+++ tasks/task008/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+if (number < 2)
+{
+    Console.WriteLine("четных чисел в диапазоне нет");
+}
+else
+{
+    Console.Write("Четные числа в диапазоне от 1 до ");
+    Console.Write(number);
+    Console.WriteLine(":");
+    Console.Write("2");
+    number2 += 2;
+        while (number2 <= number)
+    {
+        Console.Write(", ");
+        Console.Write(number2);
+        number2 += 2;        
+    }
+    Console.WriteLine("");
+}
